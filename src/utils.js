b3d6815e6de596bdf9bc3d4145e0b7d00a347668
Boluwatife Oyekanmi <enitanboluwatife5@gmail.com>
2024-11-16 19:07:15 +0100

    first commit

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+const scrambleWord = (word) => {
+    const arr = word.split('');
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr.join('');
+  };
+
+  export {scrambleWord}
